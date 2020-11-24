c35830321e96671ba1e6c16e7d4748f1d70f6f8a
Jack G <jackg@Jacks-MacBook-Air.local>
2020-11-24 17:33:43 -0500

    Initial commit

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -1,6 +1,41 @@
 <script>
     import Carousel from '@beyonk/svelte-carousel'
+    import { onMount } from 'svelte'
+
+    let sliders = [
+      {
+        title: '',
+        node: null,
+        images: [
+          {
+            title: 'example',
+            url: '/images/Projects/project1/1.jpg'
+          },
+          {
+            title: 'example',
+            url: '/images/Projects/project1/2.jpg'
+          },
+          {
+            title: 'example',
+            url: '/images/Projects/project1/3.jpg'
+          },
+          {
+            title: 'example',
+            url: '/images/Projects/project1/main.jpg'
+          }
+        ],
+      }
+    ]
+
+    onMount( () =>  {
+        
+      }
+    )
 </script>
+{#each sliders as slider }
+<div bind:this={slider.node}>
+
+
 
 <Carousel perPage=1>
     <span class="control" slot="left-control">
@@ -8,18 +43,21 @@
             <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"/>
           </svg>
     </span>
-    <div class="slide-content">Slide 1</div>
-    <div class="slide-content">Slide 2</div>
-    <div class="slide-content">Slide 3</div>
-    <div class="slide-content">Slide 4</div>
-    <div class="slide-content">Slide 5</div>
-    <div class="slide-content">Slide 6</div>
+
+    {#each slider.images as image}
+      <div class="slide-content">
+        <img src={image.url} alt={image.title}/>
+      </div>
+    {/each}
+
     <span class="control" slot="right-control">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
         <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"/>
       </svg>
     </span>
   </Carousel>
+</div>
+  {/each}
 
     <style>
 
